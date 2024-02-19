a239598dffe5c972296bd901302481bf79468ed6
Chonakan <cshuhand@gmail.com>
2024-02-20 00:45:01 +0700

    Fixes

--- frontend/vite-project/src/components/create_button.jsx
+++ frontend/vite-project/src/components/create_button.jsx
@@ -9,7 +9,7 @@ const CreateButton = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://api:80/queue/create', {
+      const response = await fetch('http://api:32409/queue/create', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
